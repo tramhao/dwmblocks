33458f7ea9f41e805990eff2e7bb631197efb318
tramhao <haoxinkui@gmail.com>
2021-03-02 20:54:40 +0800

    config.h changes

--- config.h
+++ config.h
@@ -3,17 +3,17 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-tasks",	10,	26},
+	// {"",	"sb-tasks",	10,	26},
 	// {"",	"sb-music",	0,	11},
 	{"",	"sb-pacpackages",	0,	8},
-	{"",	"sb-mailbox",	180,	12},
+	{"",	"sb-mailbox",	0,	12},
 	{"",	"sb-news",		0,	6},
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
 	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
 	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
 	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
+	// {"",	"sb-price btc Bitcoin 💰",				9000,	21},
 	// {"",	"sb-torrent",	20,	7},
 	{"",	"sb-cpubars",		2,	17},
 	{"",	"sb-memory",	10,	14},
@@ -21,8 +21,8 @@ static const Block blocks[] = {
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	// {"",	"sb-forecast",	18000,	5},
 	{"",	"sb-openweathermap",	18000,	5},
-	{"",	"sb-nettraf",	1,	16},
-	{"",	"sb-internet",	5,	4},
+	{"",	"sb-nettraf",	5,	16},
+	{"",	"sb-internet",	30,	4},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	60,	3},
 	{"",	"sb-clock",	60,	1},
@@ -31,7 +31,7 @@ static const Block blocks[] = {
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = "   ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
